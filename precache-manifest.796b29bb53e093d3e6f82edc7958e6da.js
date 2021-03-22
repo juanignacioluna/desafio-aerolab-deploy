a0b696c0a1321ab7f0953db9dcd7ba616052c071
juanignacioluna <juluna@frba.utn.edu.ar>
2021-03-22 06:38:52 -0300

    Updates

---
+++ precache-manifest.796b29bb53e093d3e6f82edc7958e6da.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ada87954ae9f0b8a038f7e2763de121",
+    "url": "/desafio-aerolab-deploy/index.html"
+  },
+  {
+    "revision": "03b6ff169b0fa3430a27",
+    "url": "/desafio-aerolab-deploy/static/css/main.6f9d49dd.chunk.css"
+  },
+  {
+    "revision": "a3685790e9b31c94fae1",
+    "url": "/desafio-aerolab-deploy/static/js/2.3830b0aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/desafio-aerolab-deploy/static/js/2.3830b0aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03b6ff169b0fa3430a27",
+    "url": "/desafio-aerolab-deploy/static/js/main.c8fc7ed0.chunk.js"
+  },
+  {
+    "revision": "9722d80f36b770c67506",
+    "url": "/desafio-aerolab-deploy/static/js/runtime-main.e4d9198c.js"
+  },
+  {
+    "revision": "71af6f4be21b71f7b37a986aca3a5af6",
+    "url": "/desafio-aerolab-deploy/static/media/aerolab-logo.71af6f4b.svg"
+  },
+  {
+    "revision": "2e6efe58997cf9953ce94c4829d49f8e",
+    "url": "/desafio-aerolab-deploy/static/media/arrow-left.2e6efe58.svg"
+  },
+  {
+    "revision": "bedef3832219792f30be7f676dffa61a",
+    "url": "/desafio-aerolab-deploy/static/media/arrow-right.bedef383.svg"
+  },
+  {
+    "revision": "2f11a957b72ff4a5c46227027cf46115",
+    "url": "/desafio-aerolab-deploy/static/media/buy-blue.2f11a957.svg"
+  },
+  {
+    "revision": "40bc281346af2ba434a9d9792491715d",
+    "url": "/desafio-aerolab-deploy/static/media/buy-white.40bc2813.svg"
+  },
+  {
+    "revision": "4669120cae147258c9fc868719b1d3ca",
+    "url": "/desafio-aerolab-deploy/static/media/coin.4669120c.svg"
+  },
+  {
+    "revision": "a343e15346be343af7867fa8f95ad9d5",
+    "url": "/desafio-aerolab-deploy/static/media/header-x2.a343e153.png"
+  }
+]);
